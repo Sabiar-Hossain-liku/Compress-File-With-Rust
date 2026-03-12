59d92b34568c8b3d715142ae0e6f903c30b81593
Sabiar-Hossain-liku <hossaqinkabir@gmail.com>
2026-03-12 14:36:42 +0600

    compression logic added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate flate2;
+
+use flate2::Compression;
+use flate2::write::GzEncoder;
+use std::env::args; // taking cli arguments
+use std::fs::File;
+use std::io::BufReader;
+use std::io::copy;
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    if args().len() != 3 {
+        println!("Usage: `source` `target`");
+        return;
+    }
+    let mut input = BufReader::new(File::open(args().nth(1).unrape()).unwrap());
+    let output = File::create(args().nth(2).unwrap()).unwrap();
+    let mut encoder = GzEncoder::new(output, Compression::default());
+
+    // for checking time that take.
+    let start = Instant::now();
+
+    copy(&mut input, &mut encoder).unwrap();
+    let output = encoder.finish().unwrap();
+
+    println!("Sourcxe len:{:?", input.get_ref().metadata.unwrap.len());
+    println!("Target len:{:?}", output.metadata().unwrap().len());
+    println!("Elapsed time: {:?}", start.elapsed());
 }
